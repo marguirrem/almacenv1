7f0af7c81e5066eb0b8671053d7f7cef0fdd9256
Marlon Aguirre <marlon.aguirre.dev@gmail.com>
2019-04-06 10:47:00 -0600

    agregando modelos cliente y factura

---
+++ src/main/java/org/marlonaguirre/almacen/core/model/Cliente.java
@@ -0,0 +1,72 @@
+package org.marlonaguirre.almacen.core.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="cliente")
+public class Cliente {
+        
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="nit")
+    private String nit;
+    
+    @Column(name="dpi")
+    private String dpi;
+    
+    @Column(name="nombre")
+    private String nombre;
+    
+    @Column(name="direccion")
+    private String direccion;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nit, String dpi, String nombre, String direccion) {
+        this.nit = nit;
+        this.dpi = dpi;
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public String getDpi() {
+        return dpi;
+    }
+
+    public void setDpi(String dpi) {
+        this.dpi = dpi;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
+}
---
+++ src/main/java/org/marlonaguirre/almacen/core/model/Factura.java
@@ -0,0 +1,75 @@
+package org.marlonaguirre.almacen.core.model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="factura")
+public class Factura {
+    
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="numero_factura")
+    private Long idCompra;
+    
+    @Column(name="nit")
+    private String nit;
+    
+    @Column(name="fecha")
+    private Date fecha;
+    
+    @Column(name="total")
+    private Double Total;
+
+    public Factura() {
+    }
+
+    public Factura(Long idCompra, String nit, Date fecha, Double Total) {
+        this.idCompra = idCompra;
+        this.nit = nit;
+        this.fecha = fecha;
+        this.Total = Total;
+    }
+
+    public Long getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Long idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Double getTotal() {
+        return Total;
+    }
+
+    public void setTotal(Double Total) {
+        this.Total = Total;
+    }
+    
+    
+    
+}
