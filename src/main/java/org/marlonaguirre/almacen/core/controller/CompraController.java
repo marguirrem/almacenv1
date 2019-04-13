6ddca265e9d30684538d6a342265c34be98ccd7d
Marlon Aguirre <marlon.aguirre.dev@gmail.com>
2019-04-12 19:42:26 -0600

    agregando controller Compra

---
+++ src/main/java/org/marlonaguirre/almacen/core/controller/CompraController.java
@@ -0,0 +1,14 @@
+package org.marlonaguirre.almacen.core.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class CompraController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+    
+}
